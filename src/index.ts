eb7e1ef0929fc9a9c2dab0a1da3b4d1402b9344c
jafrex <jafrex01@gmail.com>
2024-01-01 21:48:34 +0500

    -Fixed Typescript code and errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-const list = document.getElementById("past-orders-list");
-const orderForm = document.getElementById("order-form");
-const formVisibilityButton = document.getElementById("create-order-button");
-const formSubmitButton = document.getElementById("form-submit-button");
-const productName = document.getElementById("product-name");
-const productQuantity = document.getElementById("product-quantity");
-const successMessage = document.getElementById("success-message");
-const failureMessage = document.getElementById("failure-message");
-const editButton = document.getElementById("edit-icon");
-const deleteButton = document.getElementById("delete-icon");
+const list: HTMLElement | null = document.getElementById("past-orders-list");
+const orderForm: HTMLFormElement | null = document.getElementById(
+  "order-form"
+) as HTMLFormElement;
+const formVisibilityButton: HTMLElement | null = document.getElementById(
+  "create-order-button"
+) as HTMLButtonElement;
+const formSubmitButton: HTMLButtonElement | null = document.getElementById(
+  "form-submit-button"
+) as HTMLButtonElement;
+const productName: HTMLInputElement | null = document.getElementById(
+  "product-name"
+) as HTMLInputElement;
+const productQuantity: HTMLInputElement | null = document.getElementById(
+  "product-quantity"
+) as HTMLInputElement;
+const successMessage: HTMLElement | null = document.getElementById(
+  "success-message"
+) as HTMLDivElement;
+const failureMessage: HTMLElement | null = document.getElementById(
+  "failure-message"
+) as HTMLDivElement;
+const editButton: HTMLElement | null = document.getElementById(
+  "edit-icon"
+) as HTMLDivElement;
+const deleteButton: HTMLElement | null = document.getElementById(
+  "delete-icon"
+) as HTMLDivElement;
 
 formVisibilityButton.addEventListener("click", () => {
   orderForm.style.display =
