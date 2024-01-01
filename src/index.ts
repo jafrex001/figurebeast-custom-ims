423d84560b4eb2c62faa30018d5574ca670693d7
jafrex <jafrex01@gmail.com>
2024-01-01 21:41:48 +0500

    -Added code to ts file (Errors not fixed)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Test");
+const list = document.getElementById("past-orders-list");
+const orderForm = document.getElementById("order-form");
+const formVisibilityButton = document.getElementById("create-order-button");
+const formSubmitButton = document.getElementById("form-submit-button");
+const productName = document.getElementById("product-name");
+const productQuantity = document.getElementById("product-quantity");
+const successMessage = document.getElementById("success-message");
+const failureMessage = document.getElementById("failure-message");
+const editButton = document.getElementById("edit-icon");
+const deleteButton = document.getElementById("delete-icon");
+
+formVisibilityButton.addEventListener("click", () => {
+  orderForm.style.display =
+    orderForm.style.display === "flex" ? "none" : "flex";
+  successMessage.style.display = "none";
+  failureMessage.style.display = "none";
+});
+
+orderForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (productName.value.trim() !== "" && productQuantity.value.trim() !== "") {
+    successMessage.style.display = "flex";
+    failureMessage.style.display = "none";
+    orderForm.style.display = "none";
+    productName.value = "";
+    productQuantity.value = "";
+  } else {
+    successMessage.style.display = "none";
+    failureMessage.style.display = "flex";
+  }
+});
+
+editButton.addEventListener("click", () => {
+  console.log("test");
+});
+
+deleteButton.addEventListener("click", () => {
+  console.log("test");
+});
