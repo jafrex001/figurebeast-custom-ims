76d0250eda4a6d1cbde94919eb360913c6320c29
jafrex <jafrex01@gmail.com>
2024-01-02 21:12:09 +0500

    -Added sidebar icons in navigation

--- src/header.php
+++ src/header.php
@@ -1,9 +1,9 @@
 <div class="navigation">
        <a href="http://localhost/figurebeast-custom-ims/src/index.php"><img src="../img/Figure-Beast.svg" alt="figurebeast-logo" /></a>
         <ul>
-          <a href="index.php"><li>Dashboard</li></a>
-          <a href="past-orders.php"><li>Past Orders</li></a>
-          <a href="create-order.php"><li>Create Order</li></a>
-          <a href="view-stock.php"><li>View Current Stock</li></a>
+          <a href="index.php"><li><img src="../img/dashboard-icon.svg" alt="dashboard-icon" srcset="" class="sidebar-icon">Dashboard</li></a>
+          <a href="past-orders.php"><li><img src="../img/dashboard-icon.svg" alt="past-orders-icon" srcset="" class="sidebar-icon">Past Orders</li></a>
+          <a href="create-order.php"><li><img src="../img/dashboard-icon.svg" alt="create-orders-icon" srcset="" class="sidebar-icon">Create Order</li></a>
+          <a href="view-stock.php"><li><img src="../img/dashboard-icon.svg" alt="view-stock-icon" srcset="" class="sidebar-icon">View Current Stock</li></a>
         </ul>
       </div>
